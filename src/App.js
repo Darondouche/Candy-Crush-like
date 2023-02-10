d1fac1cf2d517ee345c436b56f3e9136997e2155
Alice <darondeau.alice@gmail.com>
2023-02-10 17:15:30 +0100

    moves and drags fonctions for the squares

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ const candyColors = ["blue", "green", "red", "orange", "yellow", "purple"];
 
 const App = () => {
   //STATE
-
   const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+  const [squareBeingDragged, setSquareBeingDragged] = useState(null);
+  const [squareBeingReplaced, setSquareBeingReplaced] = useState(null);
 
   //COMPORTEMENTS
 
@@ -16,7 +17,7 @@ const App = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const checkForColumnOfFour = () => {
     //stop at index 39 because this is the beginning of last possible column of four
-    for (let i = 0; i < 39; i++) {
+    for (let i = 0; i <= 39; i++) {
       //retrieve every possible column of four
       const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
       const decidedColor = currentColorArrangement[i];
@@ -26,8 +27,9 @@ const App = () => {
         )
       ) {
         columnOfFour.forEach(
-          (square) => (currentColorArrangement[square] = " ")
+          (square) => (currentColorArrangement[square] = "")
         );
+        return true ;
       }
     }
   };
@@ -37,7 +39,7 @@ const App = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const checkForColumnOfThree = () => {
     //stop at index 47 because this is the beginning of last possible column of three
-    for (let i = 0; i < 47; i++) {
+    for (let i = 0; i <= 47; i++) {
       //retrieve every possible column of three
       const columnOfThree = [i, i + width, i + width * 2];
       //retrieve color of the column first candy
@@ -49,8 +51,9 @@ const App = () => {
         )
       ) {
         columnOfThree.forEach(
-          (square) => (currentColorArrangement[square] = " ")
+          (square) => (currentColorArrangement[square] = "")
         );
+        return true ;
       }
     }
   };
@@ -73,8 +76,9 @@ const App = () => {
           (square) => currentColorArrangement[square] === decidedColor
         )
       ) {
-        rowOfThree.forEach((square) => (currentColorArrangement[square] = " "));
+        rowOfThree.forEach((square) => (currentColorArrangement[square] = ""));
       }
+      return true ;
     }
   };
 
@@ -97,8 +101,9 @@ const App = () => {
           (square) => currentColorArrangement[square] === decidedColor
         )
       ) {
-        rowOfFour.forEach((square) => (currentColorArrangement[square] = " "));
+        rowOfFour.forEach((square) => (currentColorArrangement[square] = ""));
       }
+      return true ;
     }
   };
 
@@ -106,7 +111,18 @@ const App = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const moveIntoSquareBelow = () => {
     //looping through grid minus the last line
-    for (let i = 0; i < 64 - width; i++) {
+    for (let i = 0; i <= 55 ; i++) {
+      //identify first row
+      const firstRow = [0, 1, 2, 3, 4, 5, 6, 7];
+      //checking if i is on first row
+      const isFirstRow = firstRow.includes(i);
+
+      //checking if its first row and if so generating some new candies
+      if (isFirstRow && currentColorArrangement[i] === "") {
+        let randomNumber = Math.floor(Math.random() * candyColors.length);
+        currentColorArrangement[i] = candyColors[randomNumber];
+      }
+
       //checking if the square below is empty then move down the candy on index i + width
       if (currentColorArrangement[i + width] === "") {
         currentColorArrangement[i + width] = currentColorArrangement[i];
@@ -114,6 +130,52 @@ const App = () => {
         currentColorArrangement[i] = "";
       }
     }
+  };
+
+  //Movement of drags action
+  const dragStart = (e) => {
+    setSquareBeingDragged(e.target) ; 
+  };
+
+  const dragDrop = (e) => {
+    setSquareBeingReplaced(e.target) ;
+  };
+
+  const dragEnd = (e) => {
+    //getting the IDs of the moving squares
+    const squareBeingDraggedID = parseInt(squareBeingDragged.getAttribute('data-id')) ;
+    const squareBeingReplacedID = parseInt(squareBeingReplaced.getAttribute('data-id')) ;
+
+    //swaping the colors between square being moved and square being dropped on 
+    currentColorArrangement[squareBeingReplacedID] = squareBeingDragged.style.backgroundColor ;
+    currentColorArrangement[squareBeingDraggedID] = squareBeingReplaced.style.backgroundColor ; 
+
+    //listing all autorized moves based on the id of the square that was picked up
+    const validMoves = [
+      squareBeingDraggedID - 1,
+      squareBeingDraggedID - width,
+      squareBeingDraggedID + 1,
+      squareBeingDraggedID + width
+    ] ; 
+    //checking if moves are valid, if not, prevent moves
+    const validMove = validMoves.includes(squareBeingReplacedID) ; 
+
+    //to ckeck if moves are autorized it should also be equal to a row or column of three or four 
+    const isAColumnOfFour = checkForColumnOfFour() ;
+    const isARowOfFour = checkForRowOfFour() ;
+    const isAColumnOfThree = checkForColumnOfThree() ;
+    const isARowOfThree = checkForRowOfThree() ;
+
+    if (squareBeingReplacedID &&
+      validMove &&
+      (isARowOfThree || isARowOfFour || isAColumnOfFour || isAColumnOfThree)) {
+      setSquareBeingDragged(null)
+      setSquareBeingReplaced(null)
+  } else {
+      currentColorArrangement[squareBeingReplacedID] = squareBeingReplaced.getAttribute('src')
+      currentColorArrangement[squareBeingDraggedID] = squareBeingDragged.getAttribute('src')
+      setCurrentColorArrangement([...currentColorArrangement])
+  }
   };
 
   //board initialization
@@ -148,7 +210,7 @@ const App = () => {
       moveIntoSquareBelow();
       //regenerate random colors
       setCurrentColorArrangement([...currentColorArrangement]);
-    }, 1000);
+    }, 100);
 
     return () => clearInterval(timer);
   }, [
@@ -170,6 +232,14 @@ const App = () => {
             key={index}
             style={{ backgroundColor: candyColor }}
             alt={candyColor}
+            data-id={index}
+            draggable={true}
+            onDragStart={dragStart}
+            onDragOver={(e) => e.preventDefault()}
+            onDragEnter={(e) => e.preventDefault()}
+            onDragLeave={(e) => e.preventDefault()}
+            onDrop={dragDrop}
+            onDragEnd={dragEnd}
           />
         ))}
       </div>
